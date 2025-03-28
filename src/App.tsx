cb10300e6072f31adc6214816f63582bdf014d52
Imam Faraz Aditya <imamaditya2211@gmail.com>
2025-03-28 11:17:12 +0700

    chore: install nativewind and configure import module resolver

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+import './global.css';
+
+function App(): React.JSX.Element {
+  return (
+    <View className="bg-red-500">
+      <Text className="mt-20">Nativewind Check</Text>
+    </View>
+  );
+}
+
+export default App;
